25206831d8eb92ec6ea6538503054dca1bd3d97b
levcencoi <innalevcenco@gmail.com>
2019-05-03 09:50:52 +0300

    Add structure of project

---
+++ src/test/java/cucumber_runner/TestRunner.java
@@ -0,0 +1,15 @@
+package cucumber_runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/features"},
+        tags = {""},
+        glue = {"hooks", "stepDefs"}
+)
+
+public class TestRunner {
+}
